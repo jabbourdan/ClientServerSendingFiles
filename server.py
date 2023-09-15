77f9b6e035e4ac855b871e50e76da172ce56887f
Jabbour Dandan <111487226+jabbourdan@users.noreply.github.com>
2023-09-15 22:22:25 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,26 +1,100 @@
 #include <iostream>
 #include <fstream>
 #include <winsock2.h>
-#include <direct.h>
 #include <io.h> // For _access
+#include <filesystem>
+#include <thread>
 
 #define PORT 4456
 #define SIZE 1024
 
-bool isValidUserID(const std::string& userId) {
+namespace fs = std::filesystem;
+
+bool isValidUserIDAndFolderExist(const std::string& userId, const std::string& basePath) {
     // Check if userID is exactly 4 characters
-    return userId.length() == 4;
+    if (userId.length() != 4) {
+        return false;
+    }
+    // Check if the user's folder already exists
+    std::string fullUserFolderPath = basePath + userId;
+    if(fs::exists(fullUserFolderPath)){
+        return false;
+    }
+    return true;
 }
 
-bool folderExists(const std::string& path) {
-    return _access(path.c_str(), 0) == 0;
+void handleClient(SOCKET newSocket, const std::string& basePath) {
+    // Receive the userID
+    char userId[SIZE];
+    recv(newSocket, userId, SIZE, 0);
+    std::string userIdStr(userId);
+
+    std::string fullUserFolderPath = basePath + userIdStr;
+
+    if (!isValidUserIDAndFolderExist(userIdStr, basePath)) {
+        std::cerr << "1003 : Invalid userID format or user folder does exist." << std::endl;
+        send(newSocket, "1003 : User folder does exist.", sizeof("1003 : User folder does exist."), 0);
+        closesocket(newSocket);
+        return;  // Return to terminate the thread
+    } else if (!fs::create_directory(fullUserFolderPath)) {
+        std::cerr << "Failed to create user folder." << std::endl;
+        send(newSocket, "1003 : Failed to create user folder.", sizeof("1003 : Failed to create user folder."), 0);
+        closesocket(newSocket);
+        return;
+    }
+
+    send(newSocket, "212 : The folder user is created", sizeof("212 : The folder user is created"), 0);
+
+    while (true) {
+        // Receive file information (filename and size) from the client
+        char fileInfo[SIZE];
+        memset(fileInfo, 0, SIZE);
+        recv(newSocket, fileInfo, SIZE, 0);
+
+        // Check if the client has indicated the end of file transfers
+        if (strcmp(fileInfo, "END") == 0) {
+            send(newSocket, "File transfer completed.", sizeof("File transfer completed."), 0);
+            break;
+        }
+
+
+        // Parse file information
+        char* filename = strtok(fileInfo, "_");
+        char* fileSizeStr = strtok(nullptr, "_");
+        long fileSize = std::stol(fileSizeStr);
+
+        std::string message = "212 : File info received for " + std::string(filename);
+        // Send the message
+        send(newSocket, message.c_str(), message.size(), 0);
+        // Receive and save the file as a binary file
+        std::string fullFilePath = fullUserFolderPath + "\\" + std::string(filename);
+        std::ofstream file(fullFilePath.c_str(), std::ios::out | std::ios::binary); // Open in binary mode
+
+        char buffer[SIZE];
+        long totalBytesReceived = 0;
+        while (totalBytesReceived < fileSize) {
+            int bytesReceived = recv(newSocket, buffer, SIZE, 0);
+            if (bytesReceived <= 0) {
+                break; // Error or end of data
+            }
+            file.write(buffer, bytesReceived);
+            totalBytesReceived += bytesReceived;
+        }
+
+        file.close();
+
+        std::cout << "File received and saved as: " << filename << std::endl;
+    }
+
+    closesocket(newSocket);
 }
 
-int main() {
+
+void startServer(const std::string& basePath) {
     WSADATA wsaData;
     if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
         std::cerr << "WSAStartup failed." << std::endl;
-        return 1;
+        return;
     }
 
     SOCKET serverSocket, newSocket;
@@ -32,7 +106,7 @@ int main() {
     if (serverSocket == INVALID_SOCKET) {
         std::cerr << "Error creating socket." << std::endl;
         WSACleanup();
-        return 1;
+        return;
     }
 
     serverAddr.sin_family = AF_INET;
@@ -44,7 +118,7 @@ int main() {
         std::cerr << "Error binding socket." << std::endl;
         closesocket(serverSocket);
         WSACleanup();
-        return 1;
+        return;
     }
 
     // Listen for incoming connections
@@ -54,73 +128,29 @@ int main() {
         std::cerr << "Error listening on port " << PORT << "." << std::endl;
         closesocket(serverSocket);
         WSACleanup();
-        return 1;
+        return;
     }
 
-    newSocket = accept(serverSocket, (struct sockaddr*)&clientAddr, &addr_size); // Accept connection
+    while (true) {  // Infinite loop to keep the server running
+        newSocket = accept(serverSocket, (struct sockaddr*)&clientAddr, &addr_size); // Accept connection
 
-    // Receive the userID
-    char userId[SIZE];
-    recv(newSocket, userId, SIZE, 0);
-    std::string userIdStr(userId);
-
-    std::string path = "C:\\Users\\jabbour.dandan\\CLionProjects\\server2\\";
-    std::string fullUserFolderPath = path + userIdStr;
-
-    if (!isValidUserID(userIdStr)) {
-        std::cerr << "Invalid userID format." << std::endl;
-        closesocket(newSocket);
-        closesocket(serverSocket);
-        WSACleanup();
-        return 1;
-    }
-
-    // Check if the user's folder already exists
-    if (!folderExists(fullUserFolderPath)) {
-        // Create the user's folder
-        if (_mkdir(fullUserFolderPath.c_str()) != 0) {
-            std::cerr << "Error creating user folder." << std::endl;
-            closesocket(newSocket);
-            closesocket(serverSocket);
-            WSACleanup();
-            return 1;
+        if (newSocket == INVALID_SOCKET) {
+            std::cerr << "Error accepting connection." << std::endl;
+            continue;  // Skip the rest of the loop and wait for the next connection
         }
+
+        // Create a new thread to handle the client
+        std::thread(handleClient, newSocket, basePath).detach();
     }
 
-    send(newSocket, "The folder user is created", sizeof("The folder user is created"), 0);
-
-    // Receive file information (filename and size) from the client
-    char fileInfo[SIZE];
-    recv(newSocket, fileInfo, SIZE, 0);
-    send(newSocket, "File info received", sizeof("File info received"), 0);
-
-    // Parse file information
-    char* filename = strtok(fileInfo, "_");
-    char* fileSizeStr = strtok(nullptr, "_");
-    long fileSize = std::stol(fileSizeStr);
-
-    // Receive and save the file as a text file
-    std::string fullFilePath = fullUserFolderPath + "\\" + std::string(filename);
-    std::ofstream file(fullFilePath.c_str(), std::ios::out | std::ios::binary); // Open in binary mode
-
-    char buffer[SIZE];
-    long totalBytesReceived = 0;
-    while (totalBytesReceived < fileSize) {
-        int bytesReceived = recv(newSocket, buffer, SIZE, 0);
-        if (bytesReceived <= 0) {
-            break; // Error or end of data
-        }
-        file.write(buffer, bytesReceived);
-        totalBytesReceived += bytesReceived;
-    }
-
-    file.close();
-    closesocket(newSocket);
+    // Cleanup code (if needed) will be executed after breaking out of the loop
     closesocket(serverSocket);
-
     WSACleanup();
+}
 
-    std::cout << "File received and saved as text file: " << filename << std::endl;
-
+int main() {
+    std::string basePath = "C:\\Users\\jabbour.dandan\\CLionProjects\\server2\\";
+    startServer(basePath);
+    std::cout << "Server stopped." << std::endl;
     return 0;
 }
